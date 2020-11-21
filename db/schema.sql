966bdb7a600f72a3eeeee571b6c2da50d4da8791
Lyly Long <lylylong@gmail.com>
2020-11-20 22:37:49 -0500

    source schema.sql & seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,14 +12,13 @@ CREATE TABLE roleTB(
     role_title VARCHAR(30) NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
     department_id INTEGER UNSIGNED NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departmentTB(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departmentTB(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employeeTB(
     id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(30) NOT NULL,
     role_id INTEGER UNSIGNED NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roleTB(id) ON DELETE SET NULL,
-    manager_id INTEGER UNSIGNED,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employeeTB(id) ON DELETE SET NULL
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roleTB(id) ON DELETE CASCADE,
+    manager_name VARCHAR(30)
 )
--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,30 @@
-USE emloyees;
+USE employees;
 
-INSERT INTO departmentTB(department_name)
-VALUES ("Office"), ("Warehouse"), ("Sales");
+INSERT INTO departmentTB
+(department_name)
+VALUES ('Office'), ('Warehouse'), ('Sales');
 
-INSERT INTO roletTB(role_title, salary, department_id)
-VALUES ("Accountant", 50000, 1), 
-("Receptionist", 35000, 1),
-("Office Manager", 60000, 1),
-("Warehouse Operator", 40000, 2), 
-("Warehouse Manager", 60000, 2),
-("Salesperson", 55000, 1),
-("Sales Manager", 60000, 1);
+INSERT INTO roleTB
+(role_title, salary, department_id)
+VALUES ('Accountant', 50000, 1), 
+('Receptionist', 35000, 1),
+('Office Manager', 60000, 1),
+('Warehouse Operator', 40000, 2), 
+('Warehouse Manager', 60000, 2),
+('Salesperson', 55000, 3),
+('Sales Manager', 60000, 3);
 
-INSERT INTO employeeTB(full_name, role_id, manager_id)
-VALUES ("Amanda Schull", 1, 3), 
-("Bridget Regan", 2, 3),
-("Claire Holt", 2, 3),
-("Daniel Sharman", 3, NULL),
-("Eric Stoltz", 4, 5), 
-("Frank Welker", 4, 5),
-("Garry Chalk", 4, 5),
-("Hugo Weaving", 5, NULL),
-("Ian Holm", 6, 7), 
-("John Hurt", 6, 7),
-("Karen Allen", 6, 7),
-("Leonardo DiCaprio", 7, NULL),
+INSERT INTO employeeTB
+(full_name, role_id, manager_name)
+VALUES ('Amanda Schull', 1, 'Daniel Sharman'), 
+('Bridget Regan', 2, 'Daniel Sharman'),
+('Claire Holt', 2, 'Daniel Sharman'),
+('Daniel Sharman', 3, NULL),
+('Eric Stoltz', 4, 'Hugo Weaving'), 
+('Frank Welker', 4, 'Hugo Weaving'),
+('Garry Chalk', 4, 'Hugo Weaving'),
+('Hugo Weaving', 5, NULL),
+('Ian Holm', 6, 'Leonardo DiCaprio'), 
+('John Hurt', 6, 'Leonardo DiCaprio'),
+('Karen Allen', 6, 'Leonardo DiCaprio'),
+('Leonardo DiCaprio', 7, NULL);
